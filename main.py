1f6d53fc3dc59afe21cfe2f8be048ef78f9b1882
user <IlyushaCirk@mail.ru>
2023-03-26 17:05:56 +0300

    Добавил новые функции

--- main.py
+++ main.py
@@ -3,25 +3,35 @@ import requests as rq
 
 app = Flask(__name__)
 
+count = 0
+
 
 @app.route('/')
 @app.route('/home')
-def home():
+def url_home():
     return "Главная страница"
 
 
 @app.route('/news')
-def news():
+def url_news():
     return "Страница с новостями"
 
 
 @app.route('/about')
-def about():
+def url_about():
     return "Сайт с новостями"
 
 
+def fibonacci_value(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+
+    return str(a)
+
+
 @app.route('/fibonacci')
-def fibonacci():
+def url_fibonacci():
     a, b = 0, 1
     ans = ""
 
@@ -33,8 +43,13 @@ def fibonacci():
     return ans
 
 
+@app.route('/fibonacci/<int:n>')
+def url_fibonacci_n(n):
+    return fibonacci_value(n)
+
+
 @app.route('/money')
-def money():
+def url_money():
     res = rq.get('https://www.cbr-xml-daily.ru/daily_json.js')
     res = res.json()['Valute']
 
@@ -52,7 +67,7 @@ def money():
 
 
 @app.route('/random')
-def random():
+def url_random():
     params = {'method': 'getQuote', 'format': 'json'}
     res = rq.get('http://api.forismatic.com/api/1.0/', params=params).json()
 
@@ -60,6 +75,14 @@ def random():
     author = res['quoteAuthor']
 
     return f'{quote}<br><br>{author}'
+
+
+@app.route('/count')
+def url_count():
+    global count
+    count += 1
+
+    return f'Вы зашли на эту страницу {count} раз(а)'
 
 
 if __name__ == "__main__":
